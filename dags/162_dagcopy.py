c7ca3bdc03d99463a9d450ec9327e59381ab1e51
gitea_admin <gitea@local.domain>
2023-09-30 17:08:58 +0300

    copy

---
+++ dags/162_dagcopy.py
@@ -0,0 +1,23 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+from airflow import DAG
+
+
+with DAG(
+    dag_id="162",
+    schedule=None,
+    start_date=datetime.now(),
+    catchup=True,
+    schedule_interval='*/2 * * * *',
+
+    tags=["cam", "162"],
+) as dag:
+  first_task_main = KubernetesPodOperator(
+    name="kubernetes_operator", 
+    image="registry.localdev.me:5000/test:latest",
+    cmds=["python"],
+    arguments=["main.py"],
+    task_id="run-pod-main",
+)
+
+first_task_main
