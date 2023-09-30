1e9759b13895f55faedc7638c4425a25097cb542
gitea_admin <gitea@local.domain>
2023-09-30 17:45:12 +0300

    ed

--- dags/162_dag.py
+++ dags/162_dag.py
@@ -5,8 +5,8 @@ from airflow import DAG
 
 with DAG(
     dag_id="162",
+    start_date=datetime(2023, 9, 30),
     schedule_interval='*/2 * * * *',
-    start_date=datetime.now(),
     catchup=False,
     max_active_runs=1,
     # schedule_interval='*/2 * * * *',
