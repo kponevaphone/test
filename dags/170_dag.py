b6e22e32fda350d29cfcf86bd6b7a19e0e3da456
devubu <testovayarabota@yandex.ru>
2024-04-06 06:13:41 +0000

    ed

--- dags/170_dag.py
+++ dags/170_dag.py
@@ -3,10 +3,6 @@ from datetime import datetime
 from airflow import DAG
 from kubernetes.client import models as k8s
 
-compute_resources = k8s.V1ResourceRequirements(
-    limits={"cpu": "800m", "memory": "3Gi"},
-    requests={"cpu": "800m", "memory": "3Gi"}
-)
 
 with DAG(
     dag_id="170",
@@ -30,12 +26,10 @@ with DAG(
     cmds=["python"],
     arguments=["cn9.py"],
     env_vars={"NVIDIA_VISIBLE_DEVICES": "all", "NVIDIA_DRIVER_CAPABILITIES":"all"},
-    # resources={'limit_memory': "250M", 'limit_cpu': "100m"}, #, 'nvidia.com/gpu':"1"},
     container_resources=k8s.V1ResourceRequirements(
         limits={"memory": "250M", "cpu": "100m"},
         resources={'limit_memory': "250M", 'limit_cpu': "100m"},
     ),
-    # resources=compute_resources,
     task_id="pod-second_task",
 )
 
