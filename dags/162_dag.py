934aed7a1258b6e5868b022b19c520f5dd10dc80
devubu <testovayarabota@yandex.ru>
2024-04-06 06:34:21 +0000

    del

--- dags/162_dag.py
+++ dags/162_dag.py
@@ -4,13 +4,13 @@ from airflow import DAG
 
 
 with DAG(
-    dag_id="162",
+    dag_id="162_dag",
     start_date=datetime(2023, 9, 30),
     schedule='*/2 * * * *',
     catchup=False,
     max_active_runs=1,
     # schedule_interval='*/2 * * * *', 
-    tags=["cam", "162"],
+    tags=["cam", "162_dag"],
 ) as dag:
   first_task_main = KubernetesPodOperator(
     name="kubernetes_operator", 
