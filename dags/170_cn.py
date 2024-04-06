42bae497ec4ad1f2f40f768c583e95fcd63aeb3d
devubu <testovayarabota@yandex.ru>
2024-04-06 07:20:00 +0000

    ed

--- dags/170_cn.py
+++ dags/170_cn.py
@@ -13,22 +13,15 @@ with DAG(
     # schedule_interval='*/2 * * * *', 
     tags=["cam", "170_cn"],
 ) as dag:
-  first_task = KubernetesPodOperator(
-    name="kubernetes_operator", 
-    image="devubu:5000/pr:latest",
-    cmds=["python"],
-    arguments=["pr9.py"],
-    task_id="pod-first_task",
-)
-  second_task = KubernetesPodOperator(
+    second_task = KubernetesPodOperator(
     name="kubernetes_operator", 
     image="devubu:5000/cn:latest",
     cmds=["python"],
     arguments=["cn9.py"],
     env_vars={"NVIDIA_VISIBLE_DEVICES": "all", "NVIDIA_DRIVER_CAPABILITIES":"all"},
     container_resources=k8s.V1ResourceRequirements(limits={"nvidia.com/gpu": "1"},),
-    tolerations = [k8s.V1Toleration(key="nvidia.com/gpu")], #operator="Equal", value="present")],
+    tolerations = [k8s.V1Toleration(key="nvidia.com/gpu", value='1')], #operator="Equal", value="present")],
     task_id="pod-second_task",
 )
 
-first_task >> second_task
+second_task
