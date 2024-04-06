a7a8a8cdb198f0d59d0c05e2e2bbcfe2b18e8f75
devubu <testovayarabota@yandex.ru>
2024-04-06 06:12:32 +0000

    ed

--- dags/170_dag.py
+++ dags/170_dag.py
@@ -31,7 +31,10 @@ with DAG(
     arguments=["cn9.py"],
     env_vars={"NVIDIA_VISIBLE_DEVICES": "all", "NVIDIA_DRIVER_CAPABILITIES":"all"},
     # resources={'limit_memory': "250M", 'limit_cpu': "100m"}, #, 'nvidia.com/gpu':"1"},
-    resources=compute_resources,
+    container_resources=k8s.V1ResourceRequirements(
+        limits={"memory": "250M", "cpu": "100m"},
+    ),
+    # resources=compute_resources,
     task_id="pod-second_task",
 )
 
