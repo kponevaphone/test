07283c63f77b12c093d3a14bddf3c3b05b53d3ff
devubu <testovayarabota@yandex.ru>
2024-04-06 10:46:11 +0000

    sdf

--- dags/di.py
+++ dags/di.py
@@ -1,56 +1,42 @@
-# gpu_test.py
-# A simple Airflow DAG to check GPU availability.
-
+from airflow.decorators import dag, task
 from datetime import datetime
 
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.decorators import task
-from airflow.utils.edgemodifier import Label
+import os
+import json
+import requests
+from kubernetes.client import models as k8s
 
-# Docker library from PIP
-import docker
+new_config ={ "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(labels={"purpose": "pod-override-example"}),
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            env=[
+                                k8s.V1EnvVar(name="STATE", value="wa")
+                                ],
+                            )
+                        ]
+                    )
+                )
+            }
 
-# Simple DAG
-with DAG(
-    dag_id="di",
-    schedule_interval="@daily", 
-    start_date=datetime(2024, 4, 6), 
-    catchup=False, 
-    tags=['di']
-) as dag:
+default_args = {
+    'start_date': datetime(2021, 1, 1)
+}
 
+@dag('k8s_executor_example', schedule_interval='@daily', default_args=default_args, catchup=False)
+def taskflow():
 
-    @task(task_id='check_gpu')
-    def start_gpu_container(**kwargs):
+    @task(executor_config=new_config)
+    def get_testing_increase():
+        """
+        Gets totalTestResultsIncrease field from Covid API for given state and returns value
+        """
+        url = 'https://covidtracking.com/api/v1/states/'
+        res = requests.get(url+'{0}/current.json'.format(os.environ['STATE']))
+        return{'testing_increase': json.loads(res.text)['totalTestResultsIncrease']}
 
-         # get the docker params from the environment
-         client = docker.from_env()
-          
-         # run the container
-         response = client.containers.run(
+    get_testing_increase()
 
-             # The container you wish to call
-             'tensorflow/tensorflow:2.7.0-gpu',
-
-             # The command to run inside the container
-             'nvidia-smi',
-
-             # Passing the GPU access
-             device_requests=[
-                 docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
-             ]
-         )
-
-         return str(response)
-
-    check_gpu = start_gpu_container()
-
-
-    # Dummy functions
-    start = DummyOperator(task_id='start')
-    end   = DummyOperator(task_id='end')
-
-
-    # Create a simple workflow
-    start >> check_gpu >> end
+dag = taskflow()
--- dags/gpu_test.py
+++ dags/gpu_test.py
@@ -23,14 +23,14 @@ with DAG(
 )  
   second_task = KubernetesPodOperator(
     name="kubernetes_operator", 
-    # cluster_context='nvidia',
+    cluster_context='nvidia',
     image="devubu:5000/cn:latest",
     cmds=["python"],
     arguments=["cn9.py"],
     env_vars={"NVIDIA_VISIBLE_DEVICES": "all", "NVIDIA_DRIVER_CAPABILITIES":"all" }, #"CUDA_VISIBLE_DEVICES":"0"
     container_resources=k8s.V1ResourceRequirements(requests={'nvidia.com/gpu': 1,}, limits={'nvidia.com/gpu': 1,}),
     # container_resources=k8s.V1ResourceRequirements(limits={"nvidia.com/gpu": 1},),
-    tolerations = [k8s.V1Toleration(key="nvidia.com/gpu", operator="Equal", value='true', effect="NoSchedule")],
+    tolerations = [k8s.V1Toleration(key="nvidia.com/gpu", operator="Equal", value='present', effect="NoSchedule")],
     # Exists
     # tolerations=[{'key': NODE_POOL, 'operator': 'Equal', 'value': 'true', 'effect': "NoSchedule"},
     #               {'key': "nvidia.com/gpu", 'operator': 'Equal','value': 'present', 'effect': "NoSchedule" }],
