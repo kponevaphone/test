c9d050ef5f9c487a01a3f75e5f24d3dbf2d9711b
devubu <testovayarabota@yandex.ru>
2024-04-06 06:19:45 +0000

    de

---
+++ dags/170_nv.py
@@ -0,0 +1,36 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+from airflow import DAG
+from kubernetes.client import models as k8s
+
+
+with DAG(
+    dag_id="170",
+    start_date=datetime(2024, 4, 4),
+    schedule='*/5 * * * *',
+    catchup=False,
+    max_active_runs=1,
+    # schedule_interval='*/2 * * * *', 
+    tags=["cam", "170"],
+) as dag:
+  first_task = KubernetesPodOperator(
+    name="kubernetes_operator", 
+    image="devubu:5000/pr:latest",
+    cmds=["python"],
+    arguments=["pr9.py"],
+    task_id="pod-first_task",
+)
+  second_task = KubernetesPodOperator(
+    name="kubernetes_operator", 
+    image="devubu:5000/cn:latest",
+    cmds=["python"],
+    arguments=["cn9.py"],
+    env_vars={"NVIDIA_VISIBLE_DEVICES": "all", "NVIDIA_DRIVER_CAPABILITIES":"all"},
+    container_resources=k8s.V1ResourceRequirements(
+        limits={"nvidia.com/gpu": "1"},
+    ),
+    # resources={'limit_memory': "250M", 'limit_cpu': "100m"},
+    task_id="pod-second_task",
+)
+
+first_task >> second_task
