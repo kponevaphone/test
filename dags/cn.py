7772dcba5db5c5d6769f12593fb53ba888265dec
devubu <testovayarabota@yandex.ru>
2024-05-17 13:12:54 +0000

    ed

---
+++ dags/cn.py
@@ -0,0 +1,30 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+from airflow import DAG
+from kubernetes.client import models as k8s
+
+
+with DAG(
+    dag_id="ids",
+    start_date=datetime(2024, 4, 4),
+    schedule='*/5 * * * *',
+    catchup=False,
+    max_active_runs=1,
+    # schedule_interval='*/2 * * * *', 
+    tags=["mi", "cn"],
+) as dag:
+  first_task = KubernetesPodOperator(
+    name="mi", 
+    image="devubu:5000/mi:latest",
+    cmds=["python"],
+    arguments=["mi5.py"],
+    task_id="first_task",
+)  
+  second_task = KubernetesPodOperator(
+    name="cn", 
+    image="devubu:5000/cn:latest",
+    cmds=["python"],
+    arguments=["cn5.py"],
+    task_id="second_task",
+)
+first_task >> second_task
